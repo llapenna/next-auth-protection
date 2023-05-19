23ad97af2c3cacbe8ccac8da921d8524e4705f7c
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-05-19 00:36:46 -0300

    feat: create getServerSideProps method class

--- withAuthProtection.ts
+++ withAuthProtection.ts
@@ -2,6 +2,8 @@ import type { AuthOptions } from 'next-auth';
 import { getServerSession } from 'next-auth';
 import { GetServerSidePropsContext as Context } from 'next';
 
+import { GetServerSideProps } from './types/getServerSideProps';
+
 class WithAuthProtection {
   /**
    * Creates a new instance of `WithAuthProtection`.
@@ -19,6 +21,33 @@ class WithAuthProtection {
   private getSession(req: Context['req'], res: Context['res']) {
     return getServerSession(req, res, this.options);
   }
+
+  /**
+   * Higher-order function that returns a new `getServerSideProps` function that checks if the user is logged in.
+   * @param getServerSidePropsArg `getServerSideProps` regular function, with the addition of a `Session` argument.
+   * @param redirect Should the function redirect to the given page if the user is not logged in?
+   * @returns A new `getServerSideProps` function that checks if the user is logged in. If not, it redirects to the given page.
+   */
+  getServerSideProps: GetServerSideProps = (
+    getServerSidePropsArg,
+    redirect = true
+  ) => {
+    return async (context) => {
+      const session = await this.getSession(context.req, context.res);
+      const redirectProp = redirect ? { redirect: this.to } : undefined;
+
+      if (!session) {
+        // User is not logged in, return to the login page
+        return {
+          redirect: redirectProp,
+          props: {},
+        };
+      }
+
+      // User is logged in, continue to the page. Also add the session to the props
+      return getServerSidePropsArg(context, session);
+    };
+  };
 
 }
 
