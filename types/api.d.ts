a3531aaaf2fa5fcc1f09abeb1da03cbdb24f2acd
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-05-18 23:06:24 -0300

    feat: add api route types

---
+++ types/api.d.ts
@@ -0,0 +1,23 @@
+import type { Session } from 'next-auth';
+import type {
+  NextApiHandler,
+  NextApiRequest,
+  NextApiResponse,
+} from 'next/types';
+
+/**
+ * API route handler with `Session` as a third argument.
+ */
+type HandlerWithSession<T = any> = (
+  req: NextApiRequest,
+  response: NextApiResponse<T>,
+  session: Session
+  // TODO check this ReturnType
+) => ReturnType<NextApiHandler<T>>;
+
+/**
+ * API route handler authentication HOC
+ */
+export type ApiHandler = <T = any>(
+  handler: HandlerWithSession<T>
+) => NextApiHandler<T>;
