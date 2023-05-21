b17d48edbbf2de110bfabd87f00b871b9191af75
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-05-21 03:38:20 -0300

    feat: add NextPage typings to page method

--- src/types/page.d.ts
+++ src/types/page.d.ts
@@ -1,21 +1,25 @@
 import type { Session } from "next-auth";
+import type { NextComponentType, NextPage, NextPageContext } from "next";
 
 import type { Options } from "./utils";
 
 /**
- * Generic React component.
+ * NextJS page component, with session as a prop.
  */
-type Component<
-  Props extends JSX.IntrinsicAttributes = JSX.IntrinsicAttributes
-> = React.FC<Props>;
+type NextPageWithSession<
+  Props extends object = object,
+  InitialProps = Props
+> = NextComponentType<
+  NextPageContext,
+  InitialProps,
+  Props & { session: Session }
+>;
 
 /**
  * Page High Order Component.
  */
-export type Page = <
-  Props extends JSX.IntrinsicAttributes = JSX.IntrinsicAttributes
->(
-  Component: Component<Props & { session: Session }>,
-  Fallback: Component,
+export type Page = <Props extends object = object, InitialProps = Props>(
+  Component: NextPageWithSession<Props, InitialProps>,
+  Fallback: React.FC,
   { redirect }: Options
-) => React.FC<Props> | null;
+) => NextPage<Props, InitialProps>;
