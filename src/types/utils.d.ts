3a627a46570b491bacafefbceaf3a81d92dcfd92
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-05-21 19:55:00 -0300

    feat: change options redirect argument type

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -7,5 +7,5 @@ export type AnyObject = { [key: string]: any };
  * Generic options for Higher Order Functions/Components.
  */
 export type Options = {
-  redirectTo?: boolean;
+  redirectTo?: string;
 };
